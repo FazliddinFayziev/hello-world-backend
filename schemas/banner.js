68f50bdcae5f91ca63302e3be0a4d9518516d6ce
FazliddinFayziev <fazrez4515@gmail.com>
2023-07-16 20:48:20 +0800

    tenth commit

---
+++ schemas/banner.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// ALL IMAGES SCHEMA
+
+const bannerSchema = new Schema({
+
+    // images
+    images: {
+        type: [String],
+        required: true,
+    },
+
+    // text
+    text: {
+        type: String,
+        required: true
+    },
+
+    // number
+    number: {
+        type: Number,
+        required: true
+    },
+
+    // category
+    category: {
+        type: String,
+        required: true
+    }
+
+})
+
+// Models
+const Banner = mongoose.model("banner", bannerSchema);
+
+// Exporting All
+exports.Banner = Banner;
